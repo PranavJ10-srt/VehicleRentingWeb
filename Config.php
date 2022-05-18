44ca3e9d7f2946789cc0dc0e08e94eee160eb76f
Pranav Jagtap <79315244+PranavJ10-srt@users.noreply.github.com>
2022-05-18 23:36:30 +0530

    Update Config.php

--- Config.php
+++ Config.php
@@ -4,10 +4,7 @@
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'dbms');
-      // $server = "localhost";
-      // $username = "root";
-      // $password = "";
-      // $dbname = "dbms";
+     
       $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 
